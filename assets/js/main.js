be2836927b28ff53c6b09dce26336485e91a98b0
webok21 <webok21@gmail.com>
2021-06-08 18:07:29 +0200

    done
    git commit -m "done"
    
    :wq
    
    x

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+];
+
+const input = document.getElementById('inputMessage');
+const text = document.getElementById('morsecodeText');
+const explanation = document.getElementById('explanation');
+
+function decode() {
+    text.innerHTML = ""
+    let uppercase = input.value.toUpperCase().split("")
+    // console.log(uppercase)
+    explanation.innerHTML = "<h1>EXPLANATION</h1>"
+    // let splittedText = uppercase.split("");
+    // console.log(splittedText)
+
+    uppercase.forEach(element => {
+        // console.log(element)
+
+        morseAlphabet.forEach(morse => {
+            // console.log(morse);
+            if (morse.letter == element) {
+                // console.log("working");
+                // console.log(morse.morseCode)
+                text.innerHTML += morse.morseCode + " "
+                explanation.innerHTML += `${element}= ${morse.morseCode}<br>`
+            }
+        })
+    })
+}
